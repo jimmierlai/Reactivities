fb794374febb3448d7beff82aa01300f193286c4
jimmielai4c <jlai@4cprojectdata.com>
2023-07-07 21:36:23 -0700

    Resolved merge conflicts

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,27 +1,13 @@
 import React from 'react';
-<<<<<<< HEAD
-import ReactDOM from 'react-dom/client';
-=======
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
->>>>>>> 5592ca1dba5331dfd4905125d8f7d27f9af62526
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-<<<<<<< HEAD
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-=======
 ReactDOM.render(
     <App />,
   document.getElementById('root')
->>>>>>> 5592ca1dba5331dfd4905125d8f7d27f9af62526
 );
 
 // If you want to start measuring performance in your app, pass a function
