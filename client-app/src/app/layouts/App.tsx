21960ca041dd64a0235e909b6790809023b4b806
jimmielai4c <jlai@4cprojectdata.com>
2023-07-21 00:17:56 -0700

    Removed some files

--- client-app/src/app/layouts/App.tsx
+++ client-app/src/app/layouts/App.tsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
 import axios from 'axios';
 import { Header, List } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
+import NavBar from './Navbar';
 
 function App() {
-const [activities, setActivities] = useState([]);
+const [activities, setActivities] = useState<Activity[]>([]);
 
 useEffect(() => {
-  axios.get('http://localhost:5000/api/activities')
+  axios.get<Activity[]>('http://localhost:5000/api/activities')
     .then(response => {
       console.log(response);
       setActivities(response.data);
@@ -16,9 +17,10 @@ useEffect(() => {
 
   return (
     <div>
-        <Header as='h2' icon='users' content='Reactivities' />
+        <NavBar />
+        
         <List>
-          {activities.map((activity: any) => (
+          {activities.map((activity) => (
             <List.Item key={activity.id}>
               {activity.title}
             </List.Item>
