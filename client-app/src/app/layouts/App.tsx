15b9dc2554f3c83b5f484d3c51a9d8defc27137c
jimmielai4c <jlai@4cprojectdata.com>
2023-07-25 01:22:35 -0700

    End of video 49

--- client-app/src/app/layouts/App.tsx
+++ client-app/src/app/layouts/App.tsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
+import { Container, Header, List } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './Navbar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import { act } from '@testing-library/react';
 
 function App() {
 const [activities, setActivities] = useState<Activity[]>([]);
@@ -16,17 +18,12 @@ useEffect(() => {
 }, [])
 
   return (
-    <div>
+    <>
         <NavBar />
-        
-        <List>
-          {activities.map((activity) => (
-            <List.Item key={activity.id}>
-              {activity.title}
-            </List.Item>
-          ))}
-        </List>
-    </div>
+        <Container style={{marginTop: '7em'}}>
+          <ActivityDashboard activities={activities} />
+        </Container>
+    </>
   );
 }
 
